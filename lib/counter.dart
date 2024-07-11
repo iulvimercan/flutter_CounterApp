705d3500070483721c9e201f002363929618ce7c
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-11 14:25:07 +0300

    buttons and text

---
+++ lib/counter.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  
+  var _counter = 0;
+
+  void decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          // make the horizontal padding quadrant as large as the vertical padding
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: Colors.purple,
+              width: 2,
+            ),
+          ),
+          child: Text(
+            '$_counter',
+            style: const TextStyle(
+              fontSize: 64,
+              color: Colors.purple,
+            ),
+          ),
+        ),
+        const SizedBox(height: 26),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: decrement,
+                icon: const Icon(
+                  size: 52,
+                  IconData(0xf038, fontFamily: 'MaterialIcons'),
+                )
+            ),
+            IconButton(
+                onPressed: increment,
+                icon: const Icon(
+                  size: 52,
+                  IconData(0xf03b, fontFamily: 'MaterialIcons'),
+                )
+            ),
+          ]
+        ),
+      ],
+    );
+  }
+  
+}
