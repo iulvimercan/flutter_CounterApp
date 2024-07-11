975de93fa9c5ae7e75d6c0ed2a20513650c871fd
iulvimercan <ibrahimulvimercan@gmail.com>
2024-07-11 14:49:49 +0300

    colors and appbar

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromARGB(255, 250, 237, 205);
+  static const secondary = Color.fromARGB(255, 212, 163, 115);
+  static const text = Color.fromARGB(255, 155, 78, 0);
+  static const light = Color.fromARGB(255, 254, 250, 224);
+  static const greenLight = Color.fromARGB(255,233, 237, 201 );
+  static const green = Color.fromARGB(255, 204, 213, 174);
+  static const purple = Colors.purple;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:counter_app/counter.dart';
+import 'package:counter_app/colors.dart';
 
 void main() {
   runApp(
-      const MaterialApp(
+      MaterialApp(
         home: Scaffold(
-          body: Center(
-            child: Counter(),
+          appBar: AppBar(
+            title: const Text(
+                'Counter App',
+                style: TextStyle(
+                  color: Colors.purple,
+                ),
+            ),
+            backgroundColor: AppColors.primary,
+          ),
+          body: Container(
+            color: AppColors.light,
+            child: const Center(
+                child: Counter()
+            ),
           ),
         ),
       ),
